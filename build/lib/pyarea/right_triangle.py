dad60bbd7cba49c78fd886b47296091246e3be98
anand mohan a m <fdfdsfdsfdsfdsfds@gmail.com>
2024-03-12 11:47:29 +0530

    pyarea

--- build/lib/pyarea/right_triangle.py
+++ build/lib/pyarea/right_triangle.py
@@ -1,4 +1,4 @@
-def right_triangle(base, height):
+def area(base, height):
     """
     Function to calculate the area of a right-angled triangle.
     
@@ -16,5 +16,5 @@ def right_triangle(base, height):
 # base_length = float(input("Enter the length of the base of the triangle: "))
 # height = float(input("Enter the height of the triangle: "))
 
-# area = right_triangle(base_length, height)
+# area = area(base_length, height)
 # print("The area of the right-angled triangle is:", area)
